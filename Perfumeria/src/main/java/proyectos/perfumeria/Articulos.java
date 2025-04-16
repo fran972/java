d1d023e37d67c19c65f5ef85ee76f2b8e4f8bc02
fran972 <blasonfran@gmail.com>
2025-04-16 19:13:58 -0300

    Update Articulos.java

--- Perfumeria/src/main/java/proyectos/perfumeria/Articulos.java
+++ Perfumeria/src/main/java/proyectos/perfumeria/Articulos.java
@@ -26,6 +26,46 @@ public class Articulos {
        this.precio=precio;
        this.stock=stock;
    }
+
+   public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
    
    
 }
