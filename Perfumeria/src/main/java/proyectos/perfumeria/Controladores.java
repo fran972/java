44c02e764a517286f4f515e9c2c83c4d08d62d1c
JulioVillegas21 <juliovillegasuba@gmail.com>
2025-04-16 20:45:09 -0300

    Add files via upload

--- Perfumeria/src/main/java/proyectos/perfumeria/Controladores.java
+++ Perfumeria/src/main/java/proyectos/perfumeria/Controladores.java
@@ -5,17 +5,27 @@ package proyectos.perfumeria;
  * @author julio
  */
 public class Controladores {
-    
-   private String identificador;
-   private String nombre;
-   private String marca;
-   private double precio;
-   private int stock;
-    
-    private void verificarCaractres (Perfumeria arreglo){
-
-        
+  
+    public boolean verificarCaractres (String identificador){
+        return identificador.matches("\\d{1,10}"); //si el texto cumple la condicion larga true, si no la cumple larga false 
     }
+    
+    public String verificarIdRepetidos (Articulos articulos[], String id ){ //verifica que le id no sea iguales a otros 
+        
+       
+        
+        
+        
+        for(Articulos a : articulos){
+            while( a.getIdentificador() == id ){
+                
+            }
+        }
+        
+        
+        return id; 
+    }
+    
     
     
     
--- Perfumeria/src/main/java/proyectos/perfumeria/Funcionalidades.java
+++ Perfumeria/src/main/java/proyectos/perfumeria/Funcionalidades.java
@@ -1,11 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectos.perfumeria;
+import java.util.Scanner;
 
 
 public class Funcionalidades {
+    
+    Scanner entrada = new Scanner(System.in);
+    Controladores verificador = new Controladores();
     
    private String identificador;
    private String nombre;
@@ -14,16 +14,20 @@ public class Funcionalidades {
    private int stock;
     
     
-    
-    
-    
-    
-    
-    public void rellenarDatos(Articulos articulo){
+
+    public void rellenarDatos(Articulos articulo[], int i){
         
         System.out.println("Porfavor indique el indentificador del nuevo articulo\n\n");
+        System.out.println("El mismo no puede superar los 10 caracteres numericos o contener letras\n\n");
         
-        articulo.setIdentificador(identificador);
+        this.identificador=entrada.nextLine();
+        
+        while (verificador.verificarCaractres(this.identificador) != true){ //llama al metodo verificar carracteres, si el mismo larga un flase, ejecuta el while
+            System.out.println("El identificador ingresado no cumple con lo pedido, porfavor vuelva a intentarlo");
+            this.identificador=entrada.nextLine();
+            
+        }
+        
         
     }
     
