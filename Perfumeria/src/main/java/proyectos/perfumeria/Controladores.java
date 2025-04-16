3b1a3c6b0fd1f5da7818aed55d554bfe3be9c466
JulioVillegas21 <juliovillegasuba@gmail.com>
2025-04-16 20:06:24 -0300

    Add files via upload

---
+++ Perfumeria/src/main/java/proyectos/perfumeria/Controladores.java
@@ -0,0 +1,29 @@
+package proyectos.perfumeria;
+
+/**
+ *
+ * @author julio
+ */
+public class Controladores {
+    
+   private String identificador;
+   private String nombre;
+   private String marca;
+   private double precio;
+   private int stock;
+    
+    private void verificarCaractres (Perfumeria arreglo){
+
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Perfumeria/src/main/java/proyectos/perfumeria/Funcionalidades.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectos.perfumeria;
+
+
+public class Funcionalidades {
+    
+   private String identificador;
+   private String nombre;
+   private String marca;
+   private double precio;
+   private int stock;
+    
+    
+    
+    
+    
+    
+    
+    public void rellenarDatos(Articulos articulo){
+        
+        System.out.println("Porfavor indique el indentificador del nuevo articulo\n\n");
+        
+        articulo.setIdentificador(identificador);
+        
+    }
+    
+    
+    
+    
+    
+    
+}
