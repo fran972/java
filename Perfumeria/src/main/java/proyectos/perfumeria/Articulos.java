c91bfe6dd52e62985c43e26aa7bad50123479ffd
JulioVillegas21 <juliovillegasuba@gmail.com>
2025-04-16 21:16:29 -0300

    Add files via upload

--- Perfumeria/src/main/java/proyectos/perfumeria/Articulos.java
+++ Perfumeria/src/main/java/proyectos/perfumeria/Articulos.java
@@ -3,14 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectos.perfumeria;
-import java.util.Scanner;
+
 /**
  *
  * @author MSI
  */
 public class Articulos {
-   Scanner scanner = new Scanner(System.in);
-   private String identificador="0";
+    
+   private String identificador;
    private String nombre;
    private String marca;
    private double precio;
@@ -19,8 +19,15 @@ public class Articulos {
    public Articulos(){
        
    }
+   public Articulos(String identificador, String nombre, String marca, double precio, int stock ){
+       this.identificador=identificador;
+       this.nombre=nombre;
+       this.marca=marca;
+       this.precio=precio;
+       this.stock=stock;
+   }
 
-    public String getIdentificador() {
+   public String getIdentificador() {
         return identificador;
     }
 
@@ -59,23 +66,6 @@ public class Articulos {
     public void setStock(int stock) {
         this.stock = stock;
     }
-   public Articulos(String identificador, String nombre, String marca, double precio, int stock ){
-       this.identificador=identificador;
-       this.nombre=nombre;
-       this.marca=marca;
-       this.precio=precio;
-       this.stock=stock;
-   }
-   
-   public void verArticulos(){
-       System.out.println("nombre: " + this.nombre);
-       System.out.println("marca: " + this.marca);
-       System.out.println("precio: " + this.precio);
-       System.out.println("Stock:" + this.stock);
-       System.out.println("Identificador: " + this.identificador);
-       System.out.println("--------------------------------------");
-   }
-   
    
    
 }
--- Perfumeria/src/main/java/proyectos/perfumeria/Controladores.java
+++ Perfumeria/src/main/java/proyectos/perfumeria/Controladores.java
@@ -1,31 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectos.perfumeria;
+import java.util.Scanner;
 
 /**
  *
- * @author MSI
+ * @author julio
  */
 public class Controladores {
-    public boolean verificarCaractres (String identificador){
+    
+    Scanner entrada = new Scanner(System.in);
+    
+    
+    
+  
+    public boolean verificarCaractresId (String identificador){
         return identificador.matches("\\d{1,10}"); //si el texto cumple la condicion larga true, si no la cumple larga false 
     }
     
-    public String verificarIdRepetidos (Articulos articulos[], String id ){ //verifica que le id no sea iguales a otros 
-        
-       
-        
-        
-        
+    public boolean verificarCaractresNombre (String identificador){
+        return identificador.matches(".{3,50}"); //si el texto cumple la condicion larga true, si no la cumple larga false 
+    }
+    
+    public boolean verificarCaractresMarca (String identificador){
+        return identificador.matches(".{3,30}"); //si el texto cumple la condicion larga true, si no la cumple larga false 
+    }
+    
+    public String verificarIdRepetidos (Articulos articulos[], String id ){ //verifica que le id no sea iguales a otros, utilizamos la recursividad para que no se pueda romper el programa 
+   
         for(Articulos a : articulos){
+            
             while( a.getIdentificador() == id ){
-                
+                System.out.println("El id que ha ingresado ya existe, porfavor ingrese otro\n");
+                id = entrada.nextLine();
+                verificarIdRepetidos(articulos,id); 
             }
         }
-        
-        
-     return id; 
+        return id; 
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
